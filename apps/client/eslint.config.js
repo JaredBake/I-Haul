c3220d55c123a6aab237e0806300ace2beb38bc2
Samuel Redd <samredd@mac.lan>
2026-02-21 18:43:23 -0700

    added eslint

---
+++ apps/client/eslint.config.js
@@ -0,0 +1,46 @@
+import js from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+        React: "readonly",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_" },
+      ],
+      "no-unused-vars": "off",
+      "no-undef": "off", // TypeScript handles this
+    },
+  },
+  {
+    ignores: [
+      "dist/",
+      "node_modules/",
+      "*.config.js",
+      "*.config.ts",
+      "*.config.cjs",
+    ],
+  },
+];
